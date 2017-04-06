e861597ae2cabfc3a1b286ddc487b991325130b8
aedanflynn <lcolla@wisc.edu>
2017-04-06 18:58:50 -0500

    Implemented User Authenication with methods of creating a session, checking associated ID and updating expiry time.

---
+++ resources/userAuth.py
@@ -0,0 +1,57 @@
+import os
+import uuid
+import mimetypes
+import datetime
+
+from pymongo import MongoClient
+from bson import ObjectId
+
+import falcon
+import json
+import msgpack
+
+class OnLogin(object):
+
+    def __init__(self, db_reference):
+        self.db = db_reference
+        self.db = MongoClient().greatLibrary
+        self.userAuth = self.db.userAuth
+        self.userAuth.create_index({"expires": 1}, {'expireAfterSeconds': 0})
+
+    def on_post(self, req, resp):
+        result = self.userAuth.insertone(
+            {
+                "uhid": req.get_param('uhid'),
+                "expires": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+             })
+        resp.data=msgpack.packb(json.dumps({'sessionID': result.inserted_id}))
+        resp.status = falcon.HTTP_201
+
+    def on_get(self, req, resp, something_else):
+        resp.status = falcon.HTTP_404
+
+class updateExpire(object):
+
+    def __init__(self, db_reference):
+        self.db = db_reference
+        self.db = MongoClient().greatLibrary
+        self.userAuth = self.db.userAuth
+
+    def incrementExpire(self):
+        self.userAuth.updateon(
+            {
+                "expires": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+            }
+        )
+
+class checkuhid(object):
+
+    def __init__(self, db_reference):
+        self.db = db_reference
+        self.db = MongoClient().greatLibrary
+        self.userAuth = self.db.userAuth
+
+    def checkid(self, currUserID, sessionID):
+        result = self.userAuth.findone({'_id': ObjectId(sessionID)},projection=['uhid'])
+        return currUserID==result.get('uhid')
+
